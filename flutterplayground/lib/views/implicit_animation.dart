d3b5282fbfbf93f7d79f910fa0ba0b3a03aa392b
Bradley Lasschuit <129509756+GemotiveerdeStudent@users.noreply.github.com>
2025-01-09 00:16:15 +0100

    Added implicit animation view

---
+++ flutterplayground/lib/views/implicit_animation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImplicitAnimationView extends StatefulWidget {
+  const ImplicitAnimationView({super.key});
+
+  @override
+  State<ImplicitAnimationView> createState() => _ImplicitAnimationViewState();
+}
+
+class _ImplicitAnimationViewState extends State<ImplicitAnimationView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
