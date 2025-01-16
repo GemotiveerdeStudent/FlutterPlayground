84ea1138c13ad3d733f80282d69cf50a4b59fa35
Bradley Lasschuit <129509756+GemotiveerdeStudent@users.noreply.github.com>
2025-01-16 20:17:29 +0100

    Added drawer view, the implementation is ment for homescreen, if i ever want to use it, grab this logic and put it in the home screen.

--- flutterplayground/lib/views/three_dimensional_drawer.dart
+++ flutterplayground/lib/views/three_dimensional_drawer.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
+
+// Code is for main page, drawer works though, fine for now. if want to be used, copy code into main page and tweek it a bit. The logic is there just not the implementation.
 
 class ThreeDimensionalDrawerView extends StatefulWidget {
   final Widget child;
@@ -29,7 +30,7 @@ class _ThreeDimensionalDrawerViewState extends State<ThreeDimensionalDrawerView>
     super.initState();
     _xControllerForChild = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 500),
+      duration: const Duration(milliseconds: 500),
     );
     _yRotationAnimationForChild = Tween<double>(
       begin: 0,
@@ -38,11 +39,11 @@ class _ThreeDimensionalDrawerViewState extends State<ThreeDimensionalDrawerView>
 
     _xControllerForDrawer = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 500),
+      duration: const Duration(milliseconds: 500),
     );
     _yRotationAnimationForDrawer = Tween<double>(
-      begin: 0,
-      end: -pi / 2,
+      begin: pi / 2.7,
+      end: 0,
     ).animate(_xControllerForDrawer);
   }
 
@@ -55,12 +56,87 @@ class _ThreeDimensionalDrawerViewState extends State<ThreeDimensionalDrawerView>
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final maxDrag = screenWidth * 0.8;
+
+    return GestureDetector(
+      onHorizontalDragUpdate: (details) {
+        final delta = details.delta.dx / maxDrag;
+        _xControllerForChild.value += delta;
+        _xControllerForDrawer.value += delta;
+      },
+      onHorizontalDragEnd: (details) {
+        if (_xControllerForChild.value < 0.5) {
+          _xControllerForChild.reverse();
+          _xControllerForDrawer.reverse();
+        } else {
+          _xControllerForChild.forward();
+          _xControllerForDrawer.forward();
+        }
+      },
+      child: AnimatedBuilder(
+        animation: Listenable.merge([
+          _xControllerForChild,
+          _xControllerForDrawer,
+        ]),
+        builder: (context, child) {
+          return Stack(
+            children: [
+              Container(
+                color: const Color(0xFF1a1b26),
+              ),
+              Transform(
+                alignment: Alignment.centerLeft,
+                transform: Matrix4.identity()
+                  ..setEntry(3, 2, 0.001)
+                  ..translate(_xControllerForChild.value * maxDrag)
+                  ..rotateY(_yRotationAnimationForChild.value),
+                child: widget.child,
+              ),
+              Transform(
+                alignment: Alignment.centerRight,
+                transform: Matrix4.identity()
+                  ..setEntry(3, 2, 0.001)
+                  ..translate(
+                      -screenWidth + _xControllerForDrawer.value * maxDrag)
+                  ..rotateY(_yRotationAnimationForDrawer.value),
+                child: widget.drawer,
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: const DrawerPage(),
+    );
+  }
+}
+
+class DrawerPage extends StatelessWidget {
+  const DrawerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return ThreeDimensionalDrawerView(
       drawer: Material(
         child: Container(
-          color: const Color(0xFF24283b),
+          color: const Color(0xff24283b),
           child: ListView.builder(
-            padding: const EdgeInsets.only(left: 0, top: 100),
+            padding: const EdgeInsets.only(left: 100, top: 100),
             itemCount: 20,
             itemBuilder: (context, index) {
               return ListTile(
@@ -74,7 +150,9 @@ class _ThreeDimensionalDrawerViewState extends State<ThreeDimensionalDrawerView>
         appBar: AppBar(
           title: const Text('Drawer'),
         ),
-        body: Container(color: const Color(0xff414868)),
+        body: Container(
+          color: const Color(0xff414868),
+        ),
       ),
     );
   }
